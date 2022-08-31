f57ad8e3ba1fb3070d879dcfa03d7790b4a127a5
rmorerahul888 <rahul.more@trionxt.com>
2022-09-01 02:02:37 +0530

    tableanalysisdata

--- main.go
+++ main.go
@@ -215,8 +215,6 @@ func Operation(usecase string, action string, applicationIdentifier string) *htt
 func StopRecordingdata(usecase string, applicationIdentifier string) *http.Response {
 	url := "https://app.devaten.com/devaten/data/stopRecording?usecaseIdentifier=" + usecase + "&inputSource=application&frocefullyStop=false"
 	method := "GET"
-	// applicationIdentifier1 := applicationIdentifier
-	// applicationIdentifier1 = strings.Replace(applicationIdentifier1, "\n", "", -1)
 
 	payload := strings.NewReader("")
 	fmt.Println(Tokenresponse.AccessToken)
@@ -254,15 +252,63 @@ func StopRecordingdata(usecase string, applicationIdentifier string) *http.Respo
 	report := gjson.Get(string(body), "reportLink")
 	fmt.Println(report)
 	lastBin := strings.LastIndex(report.String(), "view")
+
 	fmt.Println(report.String()[lastBin+5 : len(report.String())])
 	reporturl := report.String()[lastBin+5 : len(report.String())]
-	reportdata(reporturl, applicationIdentifier)
 	monitoring.RecordStopMetrics(body)
+	reportdata(reporturl, applicationIdentifier)
+	idNum := strings.Split(reporturl, "/")
 
+	fmt.Println(idNum[1])
+
+	tableanalysisdata(idNum[1], idNum[0], applicationIdentifier)
 	return res
 
 }
 
+func tableanalysisdata(idNum string, usecase string, applicationIdentifier string) *http.Response {
+	url := "https://app.devaten.com/userMgt/getTableWiseDetailsInformation?idNum=" + idNum + "&usecaseIdentifier=" + usecase
+	method := "GET"
+
+	payload := strings.NewReader("")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return &http.Response{
+			Status:     err.Error(),
+			StatusCode: 500,
+		}
+	}
+	req.Header.Add("applicationIdentifier", applicationIdentifier)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+Tokenresponse.AccessToken)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return &http.Response{
+			Status:     err.Error(),
+			StatusCode: 500,
+		}
+	}
+	defer res.Body.Close()
+	//fmt.Println(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return &http.Response{
+			Status:     err.Error(),
+			StatusCode: 500,
+		}
+	}
+	//fmt.Println(string(body))
+	monitoring.TableanalysisReport(body)
+
+	return res
+
+}
 func reportdata(usecase string, applicationIdentifier string) *http.Response {
 	url := "https://app.devaten.com/userMgt/report/" + usecase
 	method := "GET"
